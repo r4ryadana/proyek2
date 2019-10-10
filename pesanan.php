dda8215fad56c4300fd1d7519b48aea722dce779
arikyahya <arikyahya02@gmail.com>
2019-10-10 13:46:18 +0700

    pesanan a

--- pesanan.php
+++ pesanan.php
@@ -145,7 +145,7 @@ session_start();
                     <tr>
                     <br>
                     <br>
-                      <th>Kode Pesanan</th>
+                      <th>Kode Pesanan a</th>
                       <th>Nama Pelanggan</th>
                       <th>Menu</th>
                       <th>Jumlah</th>
